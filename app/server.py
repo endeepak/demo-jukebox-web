e367c409263871e74a7c2230f2423174da7800e8
Deepak N <endeep123@gmail.com>
2018-02-23 11:27:49 +0530

    Add hostname in page to demo load balancing

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_prometheus import monitor
 import requests
 import os
+import socket
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 def songs_random():
     jukebox_api_server_url = os.environ.get('JUKEBOX_API_SERVER_URL', 'http://localhost:5000')
     song = requests.get('{}/api/songs/random'.format(jukebox_api_server_url)).json()
-    return render_template('song.html', song=song)
+    return render_template('song.html', song=song, hostname=socket.gethostname())
 
 monitor(app, port=8001)
 app.run(host='0.0.0.0', port=5001)
